c92ae6cc3e093606a568f77d8fe69d31637c1b4a
TanaySharma15 <tanaysharma1526@gmail.com>
2025-06-17 22:24:52 +0530

    added some good functionality

---
+++ Client/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import Footer from "../components/Footer";
+import Listing from "../components/Listing";
+import Navbar from "../components/Navbar";
+
+function Home() {
+  return (
+    <div>
+      <Navbar />
+      <Listing />
+      <Footer />
+    </div>
+  );
+}
+
+export default Home;
